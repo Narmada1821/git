d888607286ecaa2344450968c7381000b9d63b4f
Ubuntu <ubuntu@ip-172-31-32-228.ap-south-1.compute.internal>
2025-03-28 14:15:27 +0000

    removed last line and added 1 line

--- sbi.py
+++ sbi.py
@@ -3,4 +3,4 @@ git add 1
 git status – to check the file status
 git commit -m “commit message” (files available in local repository)
 git log – it shows commit history
-git log --oneline – shows commit history in single line.
+git log --stat – it will show the statastics about the changes.
